ac0d862a32a8639d3d607bfbcf37478f46fe7adc
Christian Trott <crtrott@sandia.gov>
2022-01-21 13:49:09 -0700

    Apply Clang-Format to library files
    This will help synchronize changes done in Kokkos

--- atomics/include/desul/atomics/cuda/CUDA_asm.hpp
+++ atomics/include/desul/atomics/cuda/CUDA_asm.hpp
@@ -1,8 +1,8 @@
-#include<limits>
+#include <limits>
 namespace desul {
-#if defined(__CUDA_ARCH__)  || (defined(__clang__) && !defined(__NVCC__))
+#if defined(__CUDA_ARCH__) || (defined(__clang__) && !defined(__NVCC__))
 
-#include<desul/atomics/cuda/cuda_cc7_asm.inc>
+#include <desul/atomics/cuda/cuda_cc7_asm.inc>
 
 #endif
-}
+}  // namespace desul
