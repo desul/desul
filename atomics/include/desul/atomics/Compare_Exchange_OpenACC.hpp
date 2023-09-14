b6b41ff6b85889eca4526366bada4eb5c4a6eb68
Damien L-G <dalg24@gmail.com>
2023-09-13 22:48:57 -0400

    Reuse code when casting to unsigned type and doing CAS

--- atomics/include/desul/atomics/Compare_Exchange_OpenACC.hpp
+++ atomics/include/desul/atomics/Compare_Exchange_OpenACC.hpp
@@ -52,18 +52,14 @@ T device_atomic_exchange(T* dest, T value, MemoryOrder, MemoryScope /*scope*/) {
 template <class T, class MemoryOrder, class MemoryScope>
 T device_atomic_compare_exchange(
     T* dest, T compare, T value, MemoryOrder, MemoryScope scope) {
-  if constexpr (std::is_integral_v<T> && (sizeof(T) == 4)) {
+  if constexpr (std::is_integral_v<T> && ((sizeof(T) == 4) || (sizeof(T) == 8))) {
     static_assert(sizeof(unsigned int) == 4);
-    unsigned int return_val = atomicCAS(reinterpret_cast<unsigned int*>(dest),
-                                        reinterpret_cast<unsigned int&>(compare),
-                                        reinterpret_cast<unsigned int&>(value));
-    return reinterpret_cast<T&>(return_val);
-  } else if constexpr (std::is_integral_v<T> && (sizeof(T) == 8)) {
     static_assert(sizeof(unsigned long long int) == 8);
-    unsigned long long int return_val =
-        atomicCAS(reinterpret_cast<unsigned long long int*>(dest),
-                  reinterpret_cast<unsigned long long int&>(compare),
-                  reinterpret_cast<unsigned long long int&>(value));
+    using cas_t =
+        std::conditional_t<(sizeof(T) == 4), unsigned int, unsigned long long int>;
+    cas_t return_val = atomicCAS(reinterpret_cast<cas_t*>(dest),
+                                 reinterpret_cast<cas_t&>(compare),
+                                 reinterpret_cast<cas_t&>(value));
     return reinterpret_cast<T&>(return_val);
 #ifdef DESUL_CUDA_ARCH_IS_PRE_PASCAL
   } else if constexpr (std::is_same_v<T, float>) {
