57ae2f32507ad9e73ad3eba3774e4d68c35e2e77
Max Yang <yang39@llnl.gov>
2024-02-28 10:55:18 -0800

    Use HIP intrinsics for atomic arithmetic when possible

--- atomics/include/desul/atomics/Fetch_Op_HIP.hpp
+++ atomics/include/desul/atomics/Fetch_Op_HIP.hpp
@@ -11,19 +11,17 @@ SPDX-License-Identifier: (BSD-3-Clause)
 
 namespace desul {
 namespace Impl {
-#if __has_builtin(__hip_atomic_fetch_add) \
- && __has_builtin(__hip_atomic_fetch_min) \
- && __has_builtin(__hip_atomic_fetch_max)
-    // The above intrinsics are the minimum set needed to implement the math
-    // operators:
-    //  fetch_add, fetch_sub, fetch_min, fetch_max, fetch_inc, fetch_dec
-    #define DESUL_HAVE_HIP_ATOMIC_MATH_DETAIL
+#if __has_builtin(__hip_atomic_fetch_add) && __has_builtin(__hip_atomic_fetch_min) && \
+    __has_builtin(__hip_atomic_fetch_max)
+// The above intrinsics are the minimum set needed to implement the math
+// operators:
+//  fetch_add, fetch_sub, fetch_min, fetch_max, fetch_inc, fetch_dec
+#define DESUL_HAVE_HIP_ATOMIC_MATH_DETAIL
 #endif
-#if __has_builtin(__hip_atomic_fetch_and) \
- && __has_builtin(__hip_atomic_fetch_or) \
- && __has_builtin(__hip_atomic_fetch_xor)
-    // The above intrinsics are needed for bitwise operations on integral types.
-    #define DESUL_HAVE_HIP_ATOMIC_BIT_DETAIL
+#if __has_builtin(__hip_atomic_fetch_and) && __has_builtin(__hip_atomic_fetch_or) && \
+    __has_builtin(__hip_atomic_fetch_xor)
+// The above intrinsics are needed for bitwise operations on integral types.
+#define DESUL_HAVE_HIP_ATOMIC_BIT_DETAIL
 #endif
 
 template <typename MemoryTag>
@@ -67,6 +65,67 @@ struct DesulToBuiltin<MemoryScopeNode> {
   constexpr static auto value = __HIP_MEMORY_SCOPE_SYSTEM;
 };
 
+#ifdef DESUL_HAVE_HIP_ATOMIC_MATH_DETAIL
+template <typename T, typename MemoryOrder, typename MemoryScope>
+inline __device__ std::enable_if_t<std::is_arithmetic<T>::value, T>
+device_atomic_fetch_add(T* ptr, T val, MemoryOrder, MemoryScope) {
+  return __hip_atomic_fetch_add(
+      ptr, val, DesulToBuiltin<MemoryOrder>::value, DesulToBuiltin<MemoryScope>::value);
+}
+
+template <typename T, typename MemoryOrder, typename MemoryScope>
+inline __device__ std::enable_if_t<std::is_arithmetic<T>::value, T>
+device_atomic_fetch_sub(T* ptr, T val, MemoryOrder, MemoryScope) {
+#if __has_builtin(__hip_atomic_fetch_sub)
+  return __hip_atomic_fetch_sub(
+      ptr, val, DesulToBuiltin<MemoryOrder>::value, DesulToBuiltin<MemoryScope>::value);
+#else
+  return __hip_atomic_fetch_add(ptr,
+                                -val,
+                                DesulToBuiltin<MemoryOrder>::value,
+                                DesulToBuiltin<MemoryScope>::value);
+#endif
+}
+
+template <typename T, typename MemoryOrder, typename MemoryScope>
+inline __device__ std::enable_if_t<std::is_arithmetic<T>::value, T>
+device_atomic_fetch_min(T* ptr, T val, MemoryOrder, MemoryScope) {
+  return __hip_atomic_fetch_min(
+      ptr, val, DesulToBuiltin<MemoryOrder>::value, DesulToBuiltin<MemoryScope>::value);
+}
+
+template <typename T, typename MemoryOrder, typename MemoryScope>
+inline __device__ std::enable_if_t<std::is_arithmetic<T>::value, T>
+device_atomic_fetch_max(T* ptr, T val, MemoryOrder, MemoryScope) {
+  return __hip_atomic_fetch_max(
+      ptr, val, DesulToBuiltin<MemoryOrder>::value, DesulToBuiltin<MemoryScope>::value);
+}
+
+template <typename T, typename MemoryOrder, typename MemoryScope>
+inline __device__ std::enable_if_t<std::is_arithmetic<T>::value, T>
+device_atomic_fetch_inc(T* ptr, MemoryOrder, MemoryScope) {
+  return __hip_atomic_fetch_add(ptr,
+                                T{1},
+                                DesulToBuiltin<MemoryOrder>::value,
+                                DesulToBuiltin<MemoryScope>::value);
+}
+
+template <typename T, typename MemoryOrder, typename MemoryScope>
+inline __device__ std::enable_if_t<std::is_arithmetic<T>::value, T>
+device_atomic_fetch_dec(T* ptr, MemoryOrder, MemoryScope) {
+#if __has_builtin(__hip_atomic_fetch_sub)
+  return __hip_atomic_fetch_sub(ptr,
+                                T{1},
+                                DesulToBuiltin<MemoryOrder>::value,
+                                DesulToBuiltin<MemoryScope>::value);
+#else
+  return __hip_atomic_fetch_add(ptr,
+                                -(T{1}),
+                                DesulToBuiltin<MemoryOrder>::value,
+                                DesulToBuiltin<MemoryScope>::value);
+#endif
+}
+#else
 // clang-format off
 inline __device__                int device_atomic_fetch_add(               int* ptr,                int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr,  val); }
 inline __device__       unsigned int device_atomic_fetch_add(      unsigned int* ptr,       unsigned int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr,  val); }
@@ -88,6 +147,17 @@ inline __device__                int device_atomic_fetch_max(               int*
 inline __device__       unsigned int device_atomic_fetch_max(      unsigned int* ptr,       unsigned int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicMax(ptr,  val); }
 inline __device__ unsigned long long device_atomic_fetch_max(unsigned long long* ptr, unsigned long long val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicMax(ptr,  val); }
 
+inline __device__                int device_atomic_fetch_inc(               int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, 1   ); }
+inline __device__       unsigned int device_atomic_fetch_inc(      unsigned int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, 1u  ); }
+inline __device__ unsigned long long device_atomic_fetch_inc(unsigned long long* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, 1ull); }
+
+inline __device__                int device_atomic_fetch_dec(               int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicSub(ptr,  1  ); }
+inline __device__       unsigned int device_atomic_fetch_dec(      unsigned int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicSub(ptr,  1u ); }
+inline __device__ unsigned long long device_atomic_fetch_dec(unsigned long long* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, -1  ); }
+// clang-format on
+#endif
+
+// clang-format off
 inline __device__                int device_atomic_fetch_and(               int* ptr,                int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAnd(ptr,  val); }
 inline __device__       unsigned int device_atomic_fetch_and(      unsigned int* ptr,       unsigned int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAnd(ptr,  val); }
 inline __device__ unsigned long long device_atomic_fetch_and(unsigned long long* ptr, unsigned long long val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAnd(ptr,  val); }
@@ -99,14 +169,6 @@ inline __device__ unsigned long long device_atomic_fetch_or (unsigned long long*
 inline __device__                int device_atomic_fetch_xor(               int* ptr,                int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicXor(ptr,  val); }
 inline __device__       unsigned int device_atomic_fetch_xor(      unsigned int* ptr,       unsigned int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicXor(ptr,  val); }
 inline __device__ unsigned long long device_atomic_fetch_xor(unsigned long long* ptr, unsigned long long val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicXor(ptr,  val); }
-
-inline __device__                int device_atomic_fetch_inc(               int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, 1   ); }
-inline __device__       unsigned int device_atomic_fetch_inc(      unsigned int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, 1u  ); }
-inline __device__ unsigned long long device_atomic_fetch_inc(unsigned long long* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, 1ull); }
-
-inline __device__                int device_atomic_fetch_dec(               int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicSub(ptr,  1  ); }
-inline __device__       unsigned int device_atomic_fetch_dec(      unsigned int* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicSub(ptr,  1u ); }
-inline __device__ unsigned long long device_atomic_fetch_dec(unsigned long long* ptr,                         MemoryOrderRelaxed, MemoryScopeDevice) { return atomicAdd(ptr, -1  ); }
 
 inline __device__       unsigned int device_atomic_fetch_inc_mod(  unsigned int* ptr,       unsigned int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicInc(ptr,  val); }
 inline __device__       unsigned int device_atomic_fetch_dec_mod(  unsigned int* ptr,       unsigned int val, MemoryOrderRelaxed, MemoryScopeDevice) { return atomicDec(ptr,  val); }
@@ -137,12 +199,15 @@ inline __device__       unsigned int device_atomic_fetch_dec_mod(  unsigned int*
   DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP(OP, float)               \
   DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP(OP, double)
 
+#ifndef DESUL_HAVE_HIP_ATOMIC_MATH_DETAIL
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(min)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(max)
+#endif
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(and)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(or)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(xor)
 
+#ifndef DESUL_HAVE_HIP_ATOMIC_MATH_DETAIL
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_FLOATING_POINT(add)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(add)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_FLOATING_POINT(sub)
@@ -150,6 +215,7 @@ DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(sub)
 
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(inc)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP_INTEGRAL(dec)
+#endif
 
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP(inc_mod, unsigned int)
 DESUL_IMPL_HIP_DEVICE_ATOMIC_FETCH_OP(dec_mod, unsigned int)
