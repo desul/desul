a4a97846c0308899f4475af2128998a507b185d7
Jakob Bludau <bludauj@ornl.gov>
2025-09-23 13:11:48 -0400

    move comments

--- atomics/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
+++ atomics/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
@@ -1,7 +1,9 @@
-#include <limits>
-
 #include <cuda.h>
+
+#include <limits>
 #if (CUDA_VERSION >= 12080) && not defined(DESUL_CUDA_ARCH_IS_PRE_HOPPER)
+// CUDA 12.8 introduced 128-bit compare and swap support on Hopper+ (compute
+// capability 9.x and higher)
 #define DESUL_HAVE_CUDA_128BIT_CAS
 #endif
 
@@ -11,8 +13,7 @@ namespace Impl {
 #include <desul/atomics/cuda/cuda_cc7_asm_exchange.inc>
 
 #ifdef DESUL_HAVE_CUDA_128BIT_CAS
-// Hopper (CC90) and above has some 128 bit atomic support
 #include <desul/atomics/cuda/cuda_cc9_asm_exchange.inc>
 #endif
-}
+}  // namespace Impl
 }  // namespace desul
